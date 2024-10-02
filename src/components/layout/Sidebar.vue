c81719157a020a8fa52256d0d2823380c15dd28b
fyp-preparation <sumanpandey977nep@gmail.com>
2024-10-02 21:32:33 +0545

    Icon Added In Sidebar

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,22 +1,38 @@
 <template>
     <div v-show="isSidebarVisible"
         class="flex flex-col p-5 bg-[#4b90f1] h-full w-[15vw] transition-all duration-300 ease-in-out">
-        <h2 class="text-[#fff] text-[24px] mb-[20px]">Menu</h2>
+        <h2 class="text-[#fff] text-[24px] mb-[20px] font-bold">Sidebar</h2>
 
         <ul class="side-menu">
             <li>
-                <router-link to="/">
-                    Home
-                </router-link>
+                <div class="flex mx-10">
+                    <div><i class="fa fa-home text-white mt-4" aria-hidden="true"></i></div>
+                    <div><router-link to="/">Home</router-link></div>
+                </div>
             </li>
             <li v-if="hasPermission('role.read')">
-                <router-link to="/role">Role</router-link>
+                <div class="flex mx-10">
+                    <div><i class="fas fa-user-shield text-white mt-4" aria-hidden="true"></i></div>
+                    <div><router-link to="/role">Role</router-link></div>
+                </div>
             </li>
             <li v-if="hasPermission('user.read')">
-                <router-link to="/user">User</router-link>
+                <div class="flex mx-10">
+                    <div><i class="fa fa-users text-white mt-4" aria-hidden="true"></i></div>
+                    <div><router-link to="/user">User</router-link></div>
+                </div>
             </li>
             <li v-if="hasPermission('banner.read')">
-                <router-link to="/banner">Banner</router-link>
+                <div class="flex mx-10">
+                    <div><i class="fas fa-image text-white mt-4" aria-hidden="true"></i></div>
+                    <div><router-link to="/banner">Banner</router-link></div>
+                </div>
+            </li>
+            <li>
+                <div class="flex mx-10">
+                    <div><i class="fas fa-cog text-white mt-4" aria-hidden="true"></i></div>
+                    <div><router-link to="/setting">Setting</router-link></div>
+                </div>
             </li>
         </ul>
     </div>
@@ -58,7 +74,7 @@ export default {
     font-size: 18px;
 }
 
-.side-menu a:hover {
+.side-menu li:hover {
     background-color: rgb(255, 0, 0);
 }
 
